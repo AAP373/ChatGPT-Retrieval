6e55f1bc0059cca166a72d9612d1c7cba4214d86
AAP373 <134045487+AAP373@users.noreply.github.com>
2023-07-31 20:32:09 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-APIKEY = "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"
-DATA_DIRECTORY = "/folder/home/chatgpt-retrieval/mydata"
+APIKEY = "YOUR_API_KEY"
+DATA_DIRECTORY = "path/to/directory/which/contains/files/to/be/read"
 PERSIST = True
-folder_id = '1RN8gadHE1O8kPR3IXIDJxlnlQ6vWNLd8'
-local_directory = '/folder/home/chatgpt-retrieval/mydata'
-port_no =5006
+folder_id = 'your_google_drive_folder_id'
+local_directory = 'path/to/directory/which/contains/files/to/be/read'
+port_no = 'put_some_no.-50__ to be safe'
