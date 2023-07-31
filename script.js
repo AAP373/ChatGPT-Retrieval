d6f5afdf8b54c62da74f9216f0f48dfec7228153
AAP373 <134045487+AAP373@users.noreply.github.com>
2023-07-31 20:44:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function runScript() {
-    fetch('http://34.173.100.113:5004/run-script', { method: 'POST' })
-    .then(response => response.text())
-    .then(data => console.log(data));
-}
-
-// Assuming you have a button with id 'run-script-button'
-document.getElementById('run-script-button').addEventListener('click', runScript);
